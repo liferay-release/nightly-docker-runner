82a6ce84001d200c67db3fd57229ba02a5d4df71
Renato Rego <renato.rego@liferay.com>
2026-02-09 15:50:28 -0300

    Create Shell script to allow people use Liferay Portal + MySQL without docker compose.

---
+++ start_testathon_docker_images.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+docker stop testathon-database
+docker stop testathon-liferay
+
+docker rm testathon-database
+docker rm testathon-liferay
+
+docker network rm testathon-network
+
+docker network create testathon-network
+
+docker run -d \
+	--name testathon-database \
+	--network testathon-network \
+	-p 3306:3306 \
+	-e MYSQL_ALLOW_EMPTY_PASSWORD="yes" \
+	-e MYSQL_DATABASE=lportal \
+	--health-cmd='mysqladmin ping -h localhost' \
+	--health-retries=10 \
+	--health-timeout=20s \
+	mysql:8.0
+
+sleep 20
+
+docker run \
+	--name testathon-liferay \
+	--network testathon-network \
+	-p 8080:8080 \
+	liferay/release-candidates:testathon-2026.q1-phase-1-for-mysql
